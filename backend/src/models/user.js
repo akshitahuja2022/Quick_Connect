8c2b06f57dfb97bb32ae069b7fd45e21751561f0
Akshit Ahuja <akshitahuja1322@gmail.com>
2025-12-09 16:58:53 +0530

    Changes In UserSchema

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePic: { type: String, default: "" },
+  },
+  { timestamps: true }
+);
 
 const UserModel = mongoose.model("connect-users", userSchema);
 export default UserModel;
