544082f41955438ec96e31b05b117cda43fa7013
Akshit Ahuja <akshitahuja1322@gmail.com>
2025-12-09 16:53:27 +0530

    MongoDB Connetion

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,11 +3,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const connectDb = () => {
-  mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB Connection Successfully"))
-    .catch((err) => console.log(err));
+const connectDb = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected: ", conn.connection.host);
+  } catch (error) {
+    console.error("Error Connection to MongoDB: ", error);
+    process.exit(1);
+  }
 };
 
 export default connectDb;
