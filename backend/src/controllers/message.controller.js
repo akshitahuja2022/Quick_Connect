b2bfa8966b5182d9b1294bf1cb88fa34d290ae73
Akshit Ahuja <akshitahuja1322@gmail.com>
2025-12-11 18:27:14 +0530

    Changes in Messages

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -74,6 +74,19 @@ const sendMessage = async (req, res) => {
     const { id: receiverId } = req.params;
     const senderId = req.user.id;
 
+    if (!text && !image) {
+      return res.status(400).json({ message: "Text or image is required." });
+    }
+    if (senderId.equals(receiverId)) {
+      return res
+        .status(400)
+        .json({ message: "Cannot send message to yourself" });
+    }
+    const receiverExits = await User.exists({ _id: receiverId });
+    if (!receiverExits) {
+      return res.status(404).json({ message: "Receiver not found" });
+    }
+
     let imageUrl;
     if (image) {
       const uploadResponse = await cloudinary.uploader.upload(image);
