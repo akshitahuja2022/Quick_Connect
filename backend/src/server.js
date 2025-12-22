d95061349800648596b148afa4af6d3e423f5ea1
Akshit Ahuja <akshitahuja1322@gmail.com>
2025-12-22 12:12:55 +0530

    Handle Cors Issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Vite default
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
